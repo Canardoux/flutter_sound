9a112e8b1233283b6fb79d115fd3c3aead073e1a
hyochan <dooboolab@gmail.com>
2018-08-30 02:38:55 +0900

    Trying to mockup and test plugin. Related: https://github.com/flutter/flutter/issues/21172.

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -6,11 +6,24 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_sound/flutter_sound.dart';
+import 'package:flutter/services.dart';
 
 import 'package:flutter_sound_example/main.dart';
 
 void main() {
+  final List<MethodCall> log = <MethodCall>[];
+  MethodChannel channel = const MethodChannel('plugins.flutter_sound/flutter_sound');
+  channel.setMockMethodCallHandler((MethodCall methodCall) async {
+    log.add(methodCall);
+  });
+
+  test('Audio Recorder', () async {
+    expect(log, equals(<MethodCall>[new MethodCall('startRecorder', "")]));
+  });
+
+  // Unregister the mock handler.
+  channel.setMockMethodCallHandler(null);
+
   testWidgets('Render test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(new MyApp());
@@ -18,14 +31,9 @@ void main() {
     // Verify that platform version is retrieved.
     expect(
         find.byWidgetPredicate(
-          (Widget widget) =>
-              widget is Text && widget.data.startsWith('Flutter Sound'),
+              (Widget widget) =>
+          widget is Text && widget.data.startsWith('Flutter Sound'),
         ),
         findsOneWidget);
-  });
-  test('startRecorder', () async {
-    FlutterSound flutterSound = new FlutterSound();
-    String path = await flutterSound.startRecorder(null);
-    print('path: $path');
   });
 }
