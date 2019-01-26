3673231c9482947cfa89ae697daee46d7a43cb96
mikeyyg96 <gallegom58@gmail.com>
2019-01-26 07:55:02 -0500

    working_seeker_with_audio_example
    The seeker works with the audio source when the user presses play.

--- example/test/unit_test.dart
+++ example/test/unit_test.dart
@@ -1,8 +1,8 @@
-import 'package:test/test.dart';
+// import 'package:test/test.dart';
 
-void main() {
-  test('my first unit test', () {
-    var answer = 11;
-    expect(answer, 11);
-  });
-}
+// void main() {
+//   test('my first unit test', () {
+//     var answer = 11;
+//     expect(answer, 11);
+//   });
+// }
