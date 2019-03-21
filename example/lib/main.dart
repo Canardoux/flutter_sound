d7d04f7a78439fb9223e70eedb197dabc35ee888
Sebastian Roth <sebastian.roth@gmail.com>
2019-03-21 08:05:48 +0800

    Add basic settings dialog.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_sound/android_encoder.dart';
+import 'package:flutter_sound/ios_quality.dart';
 import 'package:intl/intl.dart' show DateFormat;
 import 'package:intl/date_symbol_data_local.dart';
 
@@ -30,6 +33,14 @@ class _MyAppState extends State<MyApp> {
   double slider_current_position = 0.0;
   double max_duration = 1.0;
 
+  TextEditingController _sampleRateController =
+      TextEditingController(text: '44100');
+  TextEditingController _numChannelsController =
+      TextEditingController(text: '2');
+  TextEditingController _bitRateController = TextEditingController(text: '');
+  AndroidEncoder _androidEncoder = AndroidEncoder.DEFAULT;
+  IosQuality _iosQuality = IosQuality.LOW;
+
   @override
   void initState() {
     super.initState();
@@ -42,7 +53,14 @@ class _MyAppState extends State<MyApp> {
 
   void startRecorder() async {
     try {
-      String path = await flutterSound.startRecorder(null);
+      String path = await flutterSound.startRecorder(
+        null,
+        sampleRate: int.tryParse(_sampleRateController.text) ?? 44100,
+        numChannels: int.tryParse(_numChannelsController.text) ?? 2,
+        bitRate: int.tryParse(_bitRateController.text),
+        androidEncoder: _androidEncoder,
+        iosQuality: _iosQuality,
+      );
       print('startRecorder: $path');
 
       _recorderSubscription = flutterSound.onRecorderStateChanged.listen((e) {
@@ -162,6 +180,24 @@ class _MyAppState extends State<MyApp> {
         ),
         body: ListView(
           children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Builder(
+                builder: (context) {
+                  return FlatButton(
+                    child: Text('CHANGE RECORDER SETTINGS'),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return _buildRecordingSettingsDialog();
+                        },
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
@@ -283,17 +319,109 @@ class _MyAppState extends State<MyApp> {
               crossAxisAlignment: CrossAxisAlignment.center,
             ),
             Container(
-              height: 56.0,
-              child: Slider(
-                value: slider_current_position,
-                min: 0.0,
-                max: max_duration,
-                onChanged: (double value) async {
-                  await flutterSound.seekToPlayer(value.toInt());
+                height: 56.0,
+                child: Slider(
+                    value: slider_current_position,
+                    min: 0.0,
+                    max: max_duration,
+                    onChanged: (double value) async {
+                      await flutterSound.seekToPlayer(value.toInt());
+                    },
+                    divisions: max_duration.toInt()))
+          ],
+        ),
+      ),
+    );
+  }
+
+  AlertDialog _buildRecordingSettingsDialog() {
+    const Map<String, AndroidEncoder> androidEncoderMap = {
+      'Default': AndroidEncoder.DEFAULT,
+      'AMR_NB': AndroidEncoder.AMR_NB,
+      'AMR_WB': AndroidEncoder.AMR_WB,
+      'AAC': AndroidEncoder.AAC,
+      'HE_AAC': AndroidEncoder.HE_AAC,
+      'AAC_ELD': AndroidEncoder.AAC_ELD,
+      'VORBIS': AndroidEncoder.VORBIS,
+    };
+
+    const Map<String, IosQuality> iosQualityMap = {
+      'Min': IosQuality.MIN,
+      'Low': IosQuality.LOW,
+      'Medium': IosQuality.MEDIUM,
+      'High': IosQuality.HIGH,
+      'Max': IosQuality.MAX,
+    };
+
+    return AlertDialog(
+      title: Text('Recording Settings'),
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text('Sample Rate'),
+            TextField(
+              controller: _sampleRateController,
+              inputFormatters: [
+                WhitelistingTextInputFormatter(RegExp(r'[0-9]')),
+              ],
+              keyboardType: TextInputType.number,
+            ),
+            Divider(),
+            Text('Num Channels'),
+            TextField(
+              controller: _numChannelsController,
+              inputFormatters: [
+                WhitelistingTextInputFormatter(RegExp(r'[0-9]')),
+              ],
+              keyboardType: TextInputType.number,
+            ),
+            Divider(),
+            Text('Bitrate'),
+            TextField(
+              controller: _bitRateController,
+              inputFormatters: [
+                WhitelistingTextInputFormatter(RegExp(r'[0-9]')),
+              ],
+              keyboardType: TextInputType.number,
+            ),
+            Divider(),
+            Text('Audio Codec (Android)'),
+            DropdownButton<AndroidEncoder>(
+              value: _androidEncoder,
+              items: androidEncoderMap.keys.map(
+                (e) {
+                  return DropdownMenuItem(
+                    child: Text(e),
+                    value: androidEncoderMap[e],
+                  );
                 },
-                divisions: max_duration.toInt(),
-              ),
-            )
+              ).toList(),
+              onChanged: (e) {
+                setState(() {
+                  _androidEncoder = e;
+                });
+              },
+            ),
+            Divider(),
+            Text('Recording Quality (iOS)'),
+            DropdownButton<IosQuality>(
+              value: _iosQuality,
+              items: iosQualityMap.keys.map(
+                (e) {
+                  return DropdownMenuItem(
+                    child: Text(e),
+                    value: iosQualityMap[e],
+                  );
+                },
+              ).toList(),
+              onChanged: (e) {
+                setState(() {
+                  _iosQuality = e;
+                });
+              },
+            ),
           ],
         ),
       ),
