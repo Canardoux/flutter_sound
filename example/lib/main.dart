00abfa7af2393b1403f5b5f5038cd0ed72385d49
bsutton <bsutton@noojee.com.au>
2020-01-28 12:02:51 +1100

    removed unused import.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:intl/date_symbol_data_local.dart';
 
 import 'dart:async';
 import 'package:flutter_sound/flutter_sound.dart';
-import 'package:flutter_sound/android_encoder.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 enum t_MEDIA
@@ -156,7 +155,7 @@ class _MyAppState extends State<MyApp> {
 
   void startPlayer() async{
     try {
-      String path = null;
+      String path ;
       if (_media == t_MEDIA.ASSET) {
           Uint8List buffer =  (await rootBundle.load(assetSample[_codec.index])).buffer.asUint8List();
           path = await flutterSound.startPlayerFromBuffer(buffer, codec: _codec,);
