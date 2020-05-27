ef84cd6ab8e38d9a05ffa21eba5182af153bbcfd
Larpoux <larpoux@canardoux.xyz>
2020-05-27 11:15:53 +0200

    Little booboo in Codec table

--- android/src/main/java/com/dooboolab/fluttersound/FlutterSoundMediaRecorder.java
+++ android/src/main/java/com/dooboolab/fluttersound/FlutterSoundMediaRecorder.java
@@ -31,8 +31,8 @@ public class FlutterSoundMediaRecorder
 	final static String             TAG                = "SoundMediaRecorder";
 
 	static int codecArray[] = {
-		MediaRecorder.AudioEncoder.AUDIO_ENCODER_AAC, // DEFAULT
 		MediaRecorder.AudioEncoder.DEFAULT,
+		MediaRecorder.AudioEncoder.AUDIO_ENCODER_AAC,
 		sdkCompat.AUDIO_ENCODER_OPUS,
 		0, // CODEC_CAF_OPUS (specific Apple)
 		0,// CODEC_MP3 (not implemented)
