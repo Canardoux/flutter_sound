e52ec9a4599080f3e87140b1266cec3dabb86404
tgarm <dingsm@gmail.com>
2020-06-10 10:59:09 +0800

    fix android deprecating, use the same channelID for BackgroundAudio and MediaStyleHelper

--- android/src/main/java/com/dooboolab/fluttersound/BackgroundAudioService.java
+++ android/src/main/java/com/dooboolab/fluttersound/BackgroundAudioService.java
@@ -528,7 +528,7 @@ public class BackgroundAudioService
 		// Request the partial wake lock permission
 		mMediaPlayer.setWakeMode( getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK );
 		// Set the media player stream type and volume
-		mMediaPlayer.setAudioStreamType( AudioManager.STREAM_MUSIC );
+		mMediaPlayer.setAudioAttributes(new AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).build());
 		mMediaPlayer.setVolume( 1.0f, 1.0f );
 		// Set the onCompletion listener
 		mMediaPlayer.setOnCompletionListener( this );
