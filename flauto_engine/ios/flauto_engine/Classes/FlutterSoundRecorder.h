f8730a7adc0c231bdb662320c3890a7f86a79cd2
Larpoux <larpoux@canardoux.xyz>
2020-10-01 15:37:23 +0200

    pod_flauto_engine_ios.sh

--- flauto_engine/ios/flauto_engine/Classes/FlutterSoundRecorder.h
+++ flauto_engine/ios/flauto_engine/Classes/FlutterSoundRecorder.h
@@ -28,7 +28,7 @@
 
 
 #import <AVFoundation/AVFoundation.h>
-
+#import "Track.h"
 
 @interface FlutterMethodCall
 {
--- flauto_engine/ios/flauto_engine/Classes/Track.h
+++ flauto_engine/ios/flauto_engine/Classes/Track.h
@@ -17,6 +17,57 @@
  */
 
 
+// this enum MUST be synchronized with lib/flutter_sound.dart and fluttersound/AudioInterface.java
+typedef enum
+{
+
+  /// This is the default codec. If used
+  /// Flutter Sound will use the files extension to guess the codec.
+  /// If the file extension doesn't match a known codec then
+  /// Flutter Sound will throw an exception in which case you need
+  /// pass one of the know codec.
+  defaultCodec,
+
+  /// AAC codec in an ADTS container
+  aacADTS,
+
+  /// OPUS in an OGG container
+  opusOGG,
+
+  /// Apple encapsulates its bits in its own special envelope
+  /// .caf instead of a regular ogg/opus (.opus).
+  /// This is completely stupid, this is Apple.
+  opusCAF,
+
+  /// For those who really insist about supporting MP3. Shame on you !
+  mp3,
+
+  /// VORBIS in an OGG container
+  vorbisOGG,
+
+  /// Linear 16 PCM, without envelope
+  pcm16,
+
+  /// Linear 16 PCM, which is a Wave file.
+  pcm16WAV,
+
+
+  /// Linear 16 PCM, which is a AIFF file
+  pcm16AIFF,
+
+  /// Linear 16 PCM, which is a CAF file
+  pcm16CAF,
+
+  /// FLAC
+  flac,
+
+  /// AAC in a MPEG4 container
+  aacMP4,
+  
+  amr,
+} t_CODEC;
+
+
 @interface FlutterStandardTypedData
 {
 // TODO
