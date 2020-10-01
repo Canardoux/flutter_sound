13ea929d4b9aba5eea752bae18479059b5eed8af
Larpoux <larpoux@canardoux.xyz>
2020-10-01 11:36:08 +0200

    pod_flauto_engine_ios.sh

--- bin/pod_flauto_engine_ios.sh
+++ bin/pod_flauto_engine_ios.sh
@@ -3,6 +3,8 @@ if [ -z "$1" ]; then
 	echo "Correct usage is $0 [VERSION]"
 	exit -1
 fi
+export v=$1
+gsed -i  's/^\( *s\.version *)/\1$v/' flauto_engine_ios.podspec
 git add .
 git commit -m 'pod_flauto_engine_ios.sh'
 git push
