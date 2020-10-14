f2d12878709070cb4b2e21aba4703bb916187d53
Larpoux <larpoux@canardoux.xyz>
2020-10-14 16:08:20 +0200

    Test publish android

---
+++ flauto_engine/android/FlautoEngine/src/main/java/com/dooboolab/FlautoEngine/Toto.java
@@ -0,0 +1,13 @@
+package com.dooboolab.FlautoEngine;
+
+//import org.junit.Test;
+
+//import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ */
+public class Toto {
+        public void zozo(){}
+}
