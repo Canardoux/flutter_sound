f3eaf7e7e5c6f8eaca1edb2e615c39b413a484c7
Larpoux <larpoux@canardoux.xyz>
2020-10-16 15:23:41 +0200

    pod_flauto_engine_android.sh : Version 0.0.1

--- bin/flauto_engine.sh
+++ bin/flauto_engine.sh
@@ -9,7 +9,7 @@ VERSION_CODE=${VERSION//./}
 gsed -i  "s/^\( *s.version *= *\).*$/\1'$VERSION'/" flauto_engine_ios.podspec
 gsed -i  "s/^\( *s.dependency *'flauto_engine_ios', *\).*$/\1'$VERSION'/" flutter_sound/ios/flutter_sound.podspec
 gsed -i  "s/^\( *versionName *\).*$/\1'$VERSION'/" flauto_engine/android/FlautoEngine/build.gradle
-gsed -i  "s/^\( *versionCode *\).*$/\1$VERSION_CODE/" flauto_engine/android/FlautoEngine/build.gradle
+gsed -i  "s/^\( *versionCode *\).*$/\11$VERSION_CODE/" flauto_engine/android/FlautoEngine/build.gradle
 gsed -i  "s/^\( *implementation 'xyz.canardoux:FlautoEngine:\).*$/\1$VERSION'/" flutter_sound/android/build.gradle
 
 git add .
--- bin/flauto_engine_android.sh
+++ bin/flauto_engine_android.sh
@@ -7,9 +7,11 @@ fi
 
 VERSION=$1
 VERSION_CODE=${VERSION//./}
-gsed -i  "s/^\( *versionName *\).*$/\1'$1'/" flauto_engine/android/FlautoEngine/build.gradle
-gsed -i  "s/^\( *versionCode *\).*$/\1$VERSION_CODE/" flauto_engine/android/FlautoEngine/build.gradle
-gsed -i  "s/^\( *implementation 'xyz.canardoux:FlautoEngine:\).*$/\1$1'/" flutter_sound/android/build.gradle
+
+gsed -i  "s/^\( *versionName *\).*$/\1'$VERSION'/" flauto_engine/android/FlautoEngine/build.gradle
+gsed -i  "s/^\( *versionCode *\).*$/\11$VERSION_CODE/" flauto_engine/android/FlautoEngine/build.gradle
+gsed -i  "s/^\( *implementation 'xyz.canardoux:FlautoEngine:\).*$/\1$VERSION'/" flutter_sound/android/build.gradle
+
 git add .
 git commit -m "pod_flauto_engine_android.sh : Version $1"
 git push
@@ -23,6 +25,7 @@ if [ $? -ne 0 ]; then
     echo "Error"
     exit -1
 fi
+exit 0
 ./gradlew publishReleasePublicationToSonatypeRepository
 if [ $? -ne 0 ]; then
     echo "Error"
