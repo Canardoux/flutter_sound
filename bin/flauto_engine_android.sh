20103b7f9c19e5ae7a7d7169cf49c70187d56daf
Larpoux <larpoux@canardoux.xyz>
2020-10-16 11:01:56 +0200

    pod_flauto_engine_android.sh : Version =123456

--- bin/flauto_engine_android.sh
+++ bin/flauto_engine_android.sh
@@ -4,8 +4,11 @@ if [ -z "$1" ]; then
         echo "Correct usage is $0 [VERSION]"
         exit -1
 fi
-export v=$1
+set v=$1
 
+set $versionCode=123456
+gsed -i  "s/^\( *versionName *\).*$/\1'$1'/" flauto_engine_ios.podspec
+gsed -i  "s/^\( *versionCode *\).*$/\1$versionCode/" flauto_engine/android/FlautoEngine/build.gradle
 
 git add .
 git commit -m "pod_flauto_engine_android.sh : Version $1"
@@ -16,4 +19,4 @@ git push --tag -f
 cd flauto_engine/android/FlautoEngine
 ./gradlew clean
 ./gradlew assemble
-./gradlew publishReleasePublicationToSonatypeRepository
+#####./gradlew publishReleasePublicationToSonatypeRepository
