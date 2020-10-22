f6a0a075d593dfa658840a01dec0ce1bd7fb4f5d
Larpoux <larpoux@canardoux.xyz>
2020-10-22 15:55:06 +0200

    TAU : Version 6.3.0+1

--- bin/new-version.sh
+++ bin/new-version.sh
@@ -29,22 +29,22 @@ VERSION_CODE=${VERSION_CODE//+/}
 bin/flavor FULL
 bin/reldev REL
 
-gsed -i  "s/^\( *s.version *= *\).*$/\1'$VERSION'/" TauEngine.podspec
-gsed -i  "s/^\( *s.dependency *'TauEngine', *\).*$/\1'$VERSION'/" flutter_sound/ios/flutter_sound.podspec
-gsed -i  "s/^\( *versionName *\).*$/\1'$VERSION'/" TauEngine/android/TauEngine/build.gradle
-gsed -i  "s/^\( *versionCode *\).*$/\11$VERSION_CODE/" TauEngine/android/TauEngine/build.gradle
+gsed -i  "s/^\( *s.version *= *\).*$/\1'$VERSION'/"                          TauEngine.podspec
+gsed -i  "s/^\( *s.dependency *'TauEngine', *\).*$/\1'$VERSION'/"            flutter_sound/ios/flutter_sound.podspec
+gsed -i  "s/^\( *versionName *\).*$/\1'$VERSION'/"                           TauEngine/android/TauEngine/build.gradle
+gsed -i  "s/^\( *versionCode *\).*$/\11$VERSION_CODE/"                       TauEngine/android/TauEngine/build.gradle
 gsed -i  "s/^\( *implementation 'xyz.canardoux:TauEngine:\).*$/\1$VERSION'/" flutter_sound/android/build.gradle
-gsed -i  "s/^\( *s.version *= *\).*$/\1'$VERSION'/" flutter_sound/ios/flutter_sound.podspec
-gsed -i  "s/^\( *version *\).*$/\1'$VERSION'/" flutter_sound/android/build.gradle
-gsed -i  "s/^\( *version: *\).*$/\1$VERSION/" flutter_sound/pubspec.yaml
-gsed -i  "s/^\( *flutter_sound_platform_interface: *\).*$/\1$VERSION/" flutter_sound/pubspec.yaml
-gsed -i  "s/^\( *version: *\).*$/\1$VERSION/" flutter_sound/example/pubspec.yaml
-gsed -i  "s/^\( *flutter_sound: *#* *\^*\).*$/\1$VERSION/" flutter_sound/example/pubspec.yaml
+gsed -i  "s/^\( *s.version *= *\).*$/\1'$VERSION'/"                          flutter_sound/ios/flutter_sound.podspec
+gsed -i  "s/^\( *version *\).*$/\1'$VERSION'/"                               flutter_sound/android/build.gradle
+gsed -i  "s/^\( *version: *\).*$/\1$VERSION/"                                flutter_sound/pubspec.yaml
+gsed -i  "s/^\( *flutter_sound_platform_interface: *\).*$/\1$VERSION/"       flutter_sound/pubspec.yaml
+gsed -i  "s/^\( *version: *\).*$/\1$VERSION/"                                flutter_sound/example/pubspec.yaml
+gsed -i  "s/^\( *flutter_sound: *#* *\^*\).*$/\1$VERSION/"                   flutter_sound/example/pubspec.yaml
 gsed -i  "s/^\( *flutter_sound_platform_interface: *#* *\^*\).*$/\1$VERSION/" flutter_sound/example/pubspec.yaml
 
 
-gsed -i  "s/^\( *flutter_sound_lite: *#* *\^*\).*$/\1$VERSION/" flutter_sound/example/pubspec.yaml
-gsed -i  "s/^\( *## \).*$/\1$VERSION/" flutter_sound/CHANGELOG.md
+gsed -i  "s/^\( *flutter_sound_lite: *#* *\^*\).*$/\1$VERSION/"              flutter_sound/example/pubspec.yaml
+gsed -i  "s/^\( *## \).*$/\1$VERSION/"                                       flutter_sound/CHANGELOG.md
 gsed -i  "s/^\( *## \).*$/\1$VERSION/" flutter_sound_platform_interface/CHANGELOG.md
 gsed -i  "s/^\( *version: *\).*$/\1$VERSION/" flutter_sound_platform_interface/pubspec.yaml
 gsed -i  "s/^\( *version *= *\).*$/\1'$VERSION'/" TauEngine/android/TauEngine/bintray.gradle
@@ -56,6 +56,7 @@ git push
 git tag -f $1
 git push --tag -f
 
+exit
 
 cd flutter_sound_platform_interface/
 flutter pub publish
