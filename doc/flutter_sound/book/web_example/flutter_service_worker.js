0f4d5b281e64268996c8e4b172fb4c48963e304d
Larpoux <larpoux@canardoux.xyz>
2020-11-08 16:55:49 +0100

    TAU : documentation Version

--- doc/flutter_sound/book/web_example/flutter_service_worker.js
+++ doc/flutter_sound/book/web_example/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "8a245b76d6658c5181e6438620456cc3",
-"index.html": "5736c6928444da82a402ca595c8f99bf",
-"/": "5736c6928444da82a402ca595c8f99bf",
+"index.html": "9ce2b50e8d9471e82b9c2fa52d39b22f",
+"/": "9ce2b50e8d9471e82b9c2fa52d39b22f",
 "js/flutter_sound/flutter_sound_recorder.js": "558111fb70bdbea5f16bf9bfb77af331",
 "js/flutter_sound/flutter_sound_player.js": "a056ad1cc1016e9cbe06f86eb6f4c08e",
 "js/flutter_sound/flutter_sound.js": "83c920f0a5f553e0070f20bf123c6926",
