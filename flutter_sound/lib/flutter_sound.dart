a30ff9ab1b32d093e92e90dc61f9283551aef1a1
Larpoux <larpoux@canardoux.xyz>
2020-11-16 15:51:23 +0100

    TAU : documentation Version

--- flutter_sound/lib/flutter_sound.dart
+++ flutter_sound/lib/flutter_sound.dart
@@ -37,7 +37,7 @@ library flutter_sound;
 
 // The interfaces to the platforms specific implementations
 // --------------------------------------------------------
-export 'package:flauto_platform_interface/flutter_sound_platform_interface.dart';
+//!!!!!!!!!!export 'package:flauto_platform_interface/flutter_sound_platform_interface.dart';
 export 'package:flauto_platform_interface/flutter_sound_player_platform_interface.dart';
 export 'package:flauto_platform_interface/flutter_sound_recorder_platform_interface.dart';
 
