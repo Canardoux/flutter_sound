87312cbedb3652773157fbe1ae8305e4e45c0c4e
Larpoux <larpoux@canardoux.xyz>
2020-12-02 20:50:21 +0100

    TAU : Version

--- bin/doc.sh
+++ bin/doc.sh
@@ -10,13 +10,6 @@ flutter pub get
 dartdoc --pretty-index-json  --output ../doc/flutter_sound/api
 cd ..
 
-
-cd  doc
-rm -r _book book ../book 2>/dev/null
-gitbook build
-mv _book ../book
-cd ..
-
 git add .
 git commit -m "TAU : Version $VERSION"
 git push
@@ -24,12 +17,3 @@ if [ ! -z "$VERSION" ]; then
         git tag -f $VERSION
         git push --tag -f
 fi
-
-git checkout gh-pages
-git merge master
-git push
-if [ ! -z "$VERSION" ]; then
-        git tag -f $VERSION
-        git push --tag -f
-fi
-git checkout master
