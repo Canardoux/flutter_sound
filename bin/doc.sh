2184eda0093b93fea0e7096f427eee420cd2c9b5
Larpoux <larpoux@canardoux.xyz>
2020-12-03 10:22:40 +0100

    TAU : Version

--- bin/doc.sh
+++ bin/doc.sh
@@ -7,7 +7,7 @@ rm -r doc/flutter_sound/api
 cd flutter_sound
 flutter clean
 flutter pub get
-dartdoc --pretty-index-json  --output ../doc/flutter_sound/api
+dartdoc --pretty-index-json  --output ../doc/flutter-sound/api
 cd ..
 
 git add .
