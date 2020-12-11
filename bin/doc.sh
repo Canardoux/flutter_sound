3c97a382f271815cbc0c4212744fcea96911172a
Larpoux <larpoux@canardoux.xyz>
2020-12-11 19:53:32 +0100

    TAU : Version

--- bin/doc.sh
+++ bin/doc.sh
@@ -3,8 +3,20 @@
 VERSION=$1
 
 
+rm -r doc/pages/flutter-sound/api
+cd flutter_sound
+flutter clean
+flutter pub get
+dartdoc --pretty-index-json  --output ../doc/pages/flutter-sound/api
+cd ..
+
+if [ ! -z "$VERSION" ]; then
+        gsed -i  "s/^tau_version:.*/tau_version: $VERSION/"                                     doc/_config.yml
+        gsed -i  "s/^\( *version: \).*/\1$VERSION/"                                             doc/_data/sidebars/mydoc_sidebar.yml
+fi
+
 echo "Front matter"
-for f in $(find doc/pages/flutter_sound/api -name '*.html' )
+for f in $(find doc/pages/flutter-sound/api -name '*.html' )
 do
         echo $f
         gsed -i  "1i ---" $f
