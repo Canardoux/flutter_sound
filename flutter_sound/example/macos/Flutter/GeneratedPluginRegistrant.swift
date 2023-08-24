fa002edc7d0df29ec017699c8cfab02f42ea77a6
thanh-vo-opun-ai <thanh.vo@opun.ai>
2023-08-24 13:23:44 -0700

    pump flutter to 3.13.1 + update all dependencies

--- flutter_sound/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_sound/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,7 @@ import Foundation
 
 import audio_session
 import just_audio
-import path_provider_macos
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
