0650ee2fd038c228e0686ae283570784576aeedc
Larpoux <larpoux@gmail.com>
2024-02-24 21:51:15 +0100

    TAU : Version 9.3.1

--- flutter_sound/example 2/android/app/src/main/java/xyz/canardoux/FlutterSound/example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package xyz.canardoux.FlutterSound.example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ flutter_sound/example/android/app/src/main/java/xyz/canardoux/fluttersound/example/MainActivity.java
@@ -0,0 +1,6 @@
+package xyz.canardoux.fluttersound.example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
